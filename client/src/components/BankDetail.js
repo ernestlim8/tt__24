10f535668dc3b9ecae6e4ab5f855a8350bd0cfbe
Ernest <ernestlim8@gmail.com>
2022-12-03 14:00:44 +0800

    refactor bank detail

--- client/src/components/BankDetail.js
+++ client/src/components/BankDetail.js
@@ -2,44 +2,26 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../components/BankDetail.module.css"
 
-function BankDetail(props) {
-
-    const [accounts, setAccounts] = useState([{
-        "id": 12345,
-        "type": "investment",
-        "balance": 123.93,
-    },
-    {
-        "id": 12211,
-        "type": "savings",
-        "balance": 321.87,
-    }]);
+function BankDetail({account, navigateToAccount}) {
 
     return (
-        <div className={styles.main}>
-            <h2>Accounts</h2>
-            {
-                accounts.map((account, id) => 
-                    <div className={styles.account} onClick={() => props.navigateToAccount(account.id)}>
-                        <div className={styles.accountId}>
-                            <span>ID: {account.id}</span>
-                        </div>
-                        <div className={styles.accountDetails}>
-                            <div className={styles.accountType}>
-                                <p>{account.type} Account</p>
-                            </div>
-                            <div className={styles.bankBalance}>
-                                <p>${account.balance}</p>
-                            </div>
-                        </div>
-                    </div>
 
-                )
-            }
-
-
+        <div className={styles.account} onClick={() => navigateToAccount(account.id)}>
+            <div className={styles.accountId}>
+                <span>ID: {account.id}</span>
+            </div>
+            <div className={styles.accountDetails}>
+                <div className={styles.accountType}>
+                    <p>{account.type} Account</p>
+                </div>
+                <div className={styles.bankBalance}>
+                    <p>${account.balance}</p>
+                </div>
+            </div>
         </div>
-    );
+
+    )
+
 }
 
 export default BankDetail;
