a2330da03885e16cd05bdae11cbeb05c4cfb2b11
ZhiJun97 <angzhijun@hotmail.com>
2022-12-03 12:32:49 +0800

    improve transactionlist and transaction component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
+import Transaction from "../components/Transaction";
 
 function Dashboard() {
     return (
       <div className="App">
         <p>Dashboard</p>
         <Navbar/>
+        <Transaction/>
         
       </div>
     );
