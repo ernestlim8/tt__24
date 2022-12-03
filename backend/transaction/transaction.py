436d1de5450895aeed4ed70016ca30d358c8fe47
weide97 <wei-de-chang-1997@hotmail.com>
2022-12-03 13:29:21 +0800

    update

--- backend/transaction/transaction.py
+++ backend/transaction/transaction.py
@@ -2,11 +2,12 @@ from flask import Flask, jsonify,request
 from flask_cors import CORS
 import transactionRepo 
 import datetime
+import math 
 app = Flask(__name__)
 CORS(app)
 
 #create transaction 
-@app.route("/transaction", methods=["POST"])
+@app.route("/user/transaction", methods=["POST"])
 def createTransaction():
     try:
         currDate = datetime.datetime.now()
@@ -35,31 +36,58 @@ def createTransaction():
         ), 500
 
 
-# #get transaction 
-# @app.route("/transaction/<accountType>/<page>")
-# def getTransaction(accountType,page):
-#     try:
-#         limit = 5
-#         offset = limit *(page-1)
-#         all_transaction = getTransaction(accountType, limit, offset)
-#         result = {}
-    
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "message": "ok"
-#             }
-#         ), 200
-#     except Exception as e:
-#         print(e)
-#         return jsonify(
-#             {
-#                 "code": 500,
-#                 "message": "server error"
-#             }
-#         ), 500
+#get transaction 
+@app.route("/user/transaction/<accountID>/<page>")
+def getTransaction(accountID,page):
+    try:
+        limit = 5
+        offset = limit *(int(page)-1)
+        all_transaction = transactionRepo.getTransaction(accountID, limit, offset)
+        print(all_transaction)
+        totalTransaction = transactionRepo.getTotalCount(accountID)
+        print(totalTransaction)
+        result = {}
+        transactionInfo =[]
+        for i in all_transaction:
+            print("here")
+            transactionInfo.append({"TransactionID":i[0],"accountID": i[1], "ReceivingAccountID":i[2], "date":i[3], "transactionAmount":float(i[4]),"comment":i[5], "scheduledTime":i[6], "transactionStatus":i[7]})
+        result["transactionInfo"] = transactionInfo
+        result ["totalPage"] = math.ceil(totalTransaction[0]/5)
+        result["totalTransaction"] = totalTransaction[0]
+        return jsonify(
+            {
+                "code": 200,
+                "message": result 
+            }
+        ), 200
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "message": "server error"
+            }
+        ), 500
 
-
+@app.route("/transaction", methods=["DELETE"])
+def deleteTransactionon():
+    try:
+        data = request.get_json()
+        transactionRepo.deleteTransaction(data["transactionID"])
+        return jsonify(
+            {
+                "code": 200,
+                "message": "OK" 
+            }
+        ), 200
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "message": "server error"
+            }
+        ), 500
 
 
 if __name__ == "__main__":
