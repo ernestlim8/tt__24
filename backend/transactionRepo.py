e739b7d79d10704f8a51786ccec8de2840d54cd7
weide97 <wei-de-chang-1997@hotmail.com>
2022-12-03 11:48:36 +0800

    Create transactionRepo.py

---
+++ backend/transactionRepo.py
@@ -0,0 +1,29 @@
+import re
+import pymysql
+import os
+from dotenv import load_dotenv
+import uuid 
+
+# def getPredictionAll(transactionInfo):
+#     tid = str(uuid.uuid4())
+#     conn = connectDB()
+#     cur = conn.cursor()
+#     cur.execute(
+#                 "INSERT INTO ScheduledTransactions"+
+#                 "(TransactionID, AccountID,ReceivingAccountID,TransactionAmount,Comment,scheduledTime, transactionStatus)"+
+#                 "VALUES ("+ conn.escape(tid)+","+ conn.escape(transactionInfo["accID"])+","+ conn.escape(transactionInfo["transactionAmt"])+","
+#                 + conn.escape(transactionInfo["comment"]) +conn.escape(transactionInfo["scheduleTime"])+conn.escape(transactionInfo['transactionStatus']), ");"  
+#             )
+#     cur.commit()
+#     conn.close()
+
+
+
+def connectDB():
+    load_dotenv()
+    return pymysql.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE")
+    )
