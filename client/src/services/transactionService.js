e90872c733f2e6070098a5a5e8e626447b6e99ff
pavz02 <pavirajan02@gmail.com>
2022-12-03 14:43:29 +0800

    service

--- client/src/services/transactionService.js
+++ client/src/services/transactionService.js
@@ -1,26 +1,70 @@
 import axios from "axios";
 const baseUrl = "/api/user/transaction";
 
+const getTransaction = async (transaction) => {
+  let data = null
+  let error = null
 
-const getAll = async () => {
-  const response = await axios.get(baseUrl);
-  return response.data;
+  try {
+    const response = await axios.post(baseUrl, transaction);
+    data = transaction.data
+  } catch (err) {
+    error = err.message()
+  }
+
+  return { data, error }
 };
 
-const create = async (newObject) => {
-  const response = await axios.post(baseUrl, newObject);
-  return response.data;
+const postDeleteTransaction = async (transaction) => {
+
+}
+
+const postNewTransaction = async (transaction) => {
+  let data = null
+  let error = null
+
+  try {
+    const response = await axios.post(baseUrl, transaction);
+    data = transaction.data
+  } catch (err) {
+    error = err.message()
+  }
+
+  return { data, error }
 };
 
-const remove = async (id) => {
-  const response = await axios.delete(`${baseUrl}/${id}`);
-  return response.data;
+const postDeleteTransaction = async (transaction) => {
+  let data = null
+  let error = null
+
+  try {
+    const response = await axios.post(baseUrl, transaction);
+    data = transaction.data
+  } catch (err) {
+    error = err.message()
+  }
+
+  return { data: data, error: error }
 };
+
+// const create = async (newObject) => {
+//   const response = await axios.post(baseUrl, newObject);
+//   return response.data;
+// };
+//
+// const remove = async (id) => {
+//   const response = await axios.delete(`${baseUrl}/${id}`);
+//   return response.data;
+// };
 
 // eslint-disable-next-line
 export default {
-  getAll,
-  create,
-  remove,
+  getTransaction,
+  postNewTransaction,
+  postDeleteTransaction
 };
+
+
+// get bank account details
+
 
